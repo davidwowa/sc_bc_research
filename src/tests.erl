57ca5d6d3526d1532c5696539f5e9e6f7c82cd73
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2017-12-05 00:26:55 +0100

    - inital commit from mac and intelij

---
+++ src/tests.erl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author Wladimir David Zakrevskyy
+%%% @copyright (C) 2017, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 05. Dez 2017 00:04
+%%%-------------------------------------------------------------------
+-module(tests).
+-author("Wladimir David Zakrevskyy").
+
+%% API
+-export([]).
